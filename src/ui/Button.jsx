5f8851d15464bff6a362a059b0974b843af603af
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-01 00:03:50 +0700

    make-ui-orderitem

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function Button({ children, disabled, to, type }) {
   const base =
-    "disabled:cursor-not-allowed; inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4";
+    "disabled:cursor-not-allowed; inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 sm:px-6 sm:py-4";
   const styles = {
-    primary: base + "px-4 py-3 md:px-6 md:py-4",
-    small: base + "px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
     secondary:
-      "disabled:cursor-not-allowed inline-block border-2 border-stone-300 rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 sm:px-6 sm:py-4 px-4 py-2.5 md:px-6 md:py-3.5",
+      "disabled:cursor-not-allowed text-sm inline-block border-2 border-stone-300 rounded-full font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 sm:px-6 sm:py-4 px-4 py-2.5 md:px-6 md:py-3.5",
   };
   if (to) {
     return (
