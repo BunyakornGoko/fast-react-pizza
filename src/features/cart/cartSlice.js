81257129d1f1c33cf545f6ac6a317c738480a67d
BunyakornGoko <bunyakorn.porn@mail.kmutt.ac.th>
2024-11-01 12:10:33 +0700

    make-flow-clear-cart

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -2,16 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 import userSlice from "../user/userSlice";
 
 const initialState = {
-  //   cart: [],
-  cart: [
-    {
-      pizzaId: 12,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
+  //   cart: [
+  //     {
+  //       pizzaId: 12,
+  //       name: "Mediterranean",
+  //       quantity: 2,
+  //       unitPrice: 16,
+  //       totalPrice: 32,
+  //     },
+  //   ],
 };
 
 const cartSlice = createSlice({
@@ -51,3 +51,13 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+//reselect
